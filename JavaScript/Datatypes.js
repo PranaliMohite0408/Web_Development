ef1851306910f46b2077b614123218bd106504ab
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-01-14 14:37:10 +0530

    JavaScript

---
+++ JavaScript/Datatypes.js
@@ -0,0 +1,33 @@
+//Datatypes => Premitive Dataypes
+
+//Number
+let data=8 //Number
+let num1 = 7.8
+console.log(typeof num1) 
+num1 = -5/0
+console.log(num1) // -Infinity
+num1 = 5/0
+console.log(num1)
+
+//String
+let user = "abcd" //String
+console.log(user)
+console.log(typeof user) // string
+let user1 = "Pranali"
+let user2 = "Mohite"
+console.log(user1 + " " + user2) 
+user = "Pranali Mohite \"Talbid\" " 
+console.log(user)
+
+//boolean
+let bool = 5>6
+console.log(bool) //false
+console.log(typeof bool) //boolean
+
+//Object
+let temp = null
+console.log(typeof temp) //object
+
+//undefine
+let temp1 
+console.log(typeof temp1) //undefine
