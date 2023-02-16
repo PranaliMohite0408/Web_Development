0346104f14d79103d010cc52da842eea64988a1f
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-02-16 16:42:04 +0530

    JavaScript File

---
+++ JavaScript/IncrementDecrement.js
@@ -0,0 +1,19 @@
+//Post Increment
+let num = 4
+let x = num++
+console.log(x, num) //4, 5
+
+//Pre Increment
+let num1 = 5
+let x1 = ++num1
+console.log(num1, x1)//6, 6
+
+//Post Decrement
+let num2 = 5
+let x2 = num2--
+console.log(num2, x2)//4,5
+
+//Pre Decrement
+let num3 = 5
+let x3 = --num3
+console.log(num3, x3)//4,4
