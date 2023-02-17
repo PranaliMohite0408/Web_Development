36d58e52d4fc14cba61e0fcb45ac29c8d1598b70
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-02-17 21:39:35 +0530

    JavaScript Program

---
+++ JavaScript/Assignemts/Assignment08/Program_01.js
@@ -0,0 +1,12 @@
+//Write a program in JavaScript to check whether given number is even or odd.
+
+let x = 10;
+
+if(x%2 === 0)
+{
+    console.log("Given Number is Even");
+}
+else
+{
+    console.log("Given Number is ODD");
+}
