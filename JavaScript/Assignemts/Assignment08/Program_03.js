278f8135de7076e9964e4fe652746e69bd6109b0
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-02-18 17:54:35 +0530

    JavaScript Program

---
+++ JavaScript/Assignemts/Assignment08/Program_03.js
@@ -0,0 +1,9 @@
+//var X = [10, 20, True, “Web Development”, 3.14]; 
+//From the given array find 4th element using array index.
+
+var X = [10, 20, true, "Web Development", 3.14]; 
+
+let value = X[4];
+
+console.log(value);
+
