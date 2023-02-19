2a30acf81b83b0f74229b0623adfcadcc8c196e7
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-02-19 22:02:41 +0530

    JavaScript Program

---
+++ JavaScript/Assignemts/Assignment08/Program_04.js
@@ -0,0 +1,10 @@
+/*
+var Y = [10.20, 10.30, 50.40, 70.50, 60.20]; 
+Change the 4th array index with the float number 100.50 and
+print newly created array on browser window and console tab.
+*/
+var Y = [10.20, 10.30, 50.40, 70.50, 60.20];
+
+Y.splice(4,0,100.50)
+
+console.log(Y)
