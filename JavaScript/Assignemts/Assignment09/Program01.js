28efed7e196ea9503ea0922461fcde10664e144a
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-02-21 21:21:46 +0530

    JavaScript Program

---
+++ JavaScript/Assignemts/Assignment09/Program01.js
@@ -0,0 +1,20 @@
+/*
+Write The Programs In JavaScript To Display Given Outputs Using For Loop.
+1) Output :-
+    11
+    22
+    33
+    44
+    55
+    66
+    77
+    88
+    99
+    110
+    121
+ */
+
+for(let i=11;i<=121;i = i + 11)
+{
+    console.log(i);
+}
