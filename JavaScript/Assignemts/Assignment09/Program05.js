2a12f69028834266cdb626b3006391f9c725ec66
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-02-24 18:48:36 +0530

    JavaScript Program

---
+++ JavaScript/Assignemts/Assignment09/Program05.js
@@ -0,0 +1,20 @@
+/*
+Dispay The Given Output Using Only One For Loop
+Output :-
+    2 20
+    4 18
+    6 16
+    8 14
+    10 12
+    12 10
+    14 8
+    16 6
+    18 4
+    20 2
+*/
+
+let j =0
+for( j=2 ; j<=20; j=j+2)
+{
+    console.log(j);
+}
