9ee415045fbf06c7a7bdb5ddda4551125fd4b459
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-04-01 11:53:46 +0530

    JS Assignment

---
+++ JavaScript/Assignemts/Assignment10/Program_01.js
@@ -0,0 +1,7 @@
+// Write Program to find maximum from 2 numbers Use ternary
+// operator: (no1 > no2 ? no1 : no2). Input numbers - 35 and 96.
+
+let no1 = 35, no2 = 96;
+
+var Result = no1 > no2 ? `${no1} is Maximum Number`: `${no2} is Maximum`;
+console.log(Result);
