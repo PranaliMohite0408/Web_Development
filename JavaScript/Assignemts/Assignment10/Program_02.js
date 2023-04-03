ced3e91e17aa47dae11006db8200581d84d7df1d
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-04-03 16:00:51 +0530

    JS Assignment

---
+++ JavaScript/Assignemts/Assignment10/Program_02.js
@@ -0,0 +1,16 @@
+//Write Program to print “WELCOME” message 10 times using while and for loop.
+
+console.log("Using for loop");
+for(let i = 1;i<=10;i++)
+{
+    console.log("Welcome"); 
+}
+
+console.log("Using while loop");
+
+let i =1;
+while(i<=10)
+{
+    console.log("Welcome");
+    i++;
+}
