1aec915981e62201cc9e77a84db36ad06df00b54
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-04-04 17:19:04 +0530

    JS Assignment

---
+++ JavaScript/Assignemts/Assignment10/Program_03.js
@@ -0,0 +1,10 @@
+// Write Program to print “WEBx86” on first line & “Infotech” on second line. 
+//Print this 10 times (use only one while loop).
+
+let i = 1;
+while(i<=10)
+{
+    console.log("WEBX86");
+    console.log("Infotech");
+    i++;
+}
