fe3e275ee4f2ef8228c8b7fe4c100199214ea05f
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-05-29 17:02:34 +0530

    Function Expression in JS

---
+++ JavaScript/Functions/FunctionExpression.js
@@ -0,0 +1,11 @@
+//Function Expression => create a function and put it into the variable
+
+function sum(a,b)
+{
+    var total = a +b;
+    console.log(total)
+}
+
+var funExp = sum(5,10);
+funExp;
+
