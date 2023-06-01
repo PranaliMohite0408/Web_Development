34448f2f492f4e7a9128d941c1fb27f05793cad6
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-06-01 10:57:35 +0530

    Math Object in js

---
+++ JavaScript/MathObject.js
@@ -0,0 +1,15 @@
+//The JS Math Object allows you to Perform mathematical tasks on numbers;
+
+console.log(Math.PI)
+
+let num = 10.8755
+console.log(Math.round(num))
+console.log(Math.pow(3,4))
+console.log(Math.sqrt(25))
+
+console.log(Math.abs(-44)) //returns the absolute(positive) value of x
+console.log(Math.abs(4-6))
+
+console.log(Math.floor(Math.random()*10))
+
+console.log(Math.trunc(4.6))
