fe832ca7d66af5565aa2cbb622b1c0859716babd
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-07-11 19:08:06 +0530

    JS File

---
+++ JavaScript/ES7.js
@@ -0,0 +1,10 @@
+//1:Array Includes
+const colors = ['red', 'orange', 'green', 'white']
+const isPresent = colors.includes('red');
+console.log(isPresent);
+
+//2:expontial Operator **
+
+console.log(2**2) //4
+console.log(2**3) //8
+
