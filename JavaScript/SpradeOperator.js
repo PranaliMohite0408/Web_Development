6264c93f7a69d374ec03ac5579c589405120fb94
PranaliMohite0408 <pranalimohite482000@gmail.com>
2023-07-14 13:13:00 +0530

    JS File

---
+++ JavaScript/SpradeOperator.js
@@ -0,0 +1,7 @@
+const colors = ['red', 'greeen', 'blue', 'white'];
+
+const mycolors = ['red', 'greeen', 'blue', 'white', 'yellow', 'black'];
+
+const mycolors2 = [...colors, 'yellow', 'black'];
+
+console.log(mycolors2);
