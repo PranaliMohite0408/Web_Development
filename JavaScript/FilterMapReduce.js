0a3643e9688f681d8a036e6b03382fd9ee4d16ea
Pranali Mohite <77424192+PranaliMohite0408@users.noreply.github.com>
2023-10-02 14:01:24 +0530

    Add files via upload

---
+++ JavaScript/FilterMapReduce.js
@@ -0,0 +1,27 @@
+//map()         return -> new array
+//reduce()      return -> sigle value
+//filter()
+//forEach()     return -> undefined
+
+// const NewArray = arr.map((values,index,arr) => {
+//     return values * values;
+// });
+// console.log(arr);
+// console.log(NewArray);
+
+
+
+// const NewFilterArray = arr.filter((value, index, arr) => {
+//     return value > 25;
+// });
+// console.log(NewFilterArray);
+
+let arr = [10, 20, 30, 40, 52, 35, 65, 45];
+
+const NewReduceArray = arr.reduce((acc, value, index, arr) => {
+     debugger;
+     return acc + value;
+ });
+
+
+ console.log(NewReduceArray);
