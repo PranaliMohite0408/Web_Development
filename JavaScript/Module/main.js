a8996f933a72851f8b70797571d4b52ffc1a2beb
Pranali Mohite <77424192+PranaliMohite0408@users.noreply.github.com>
2023-10-03 18:35:54 +0530

    Add files via upload

---
+++ JavaScript/Module/main.js
@@ -0,0 +1,17 @@
+// import { message, user } from "./main1.js";
+// //console.log(message)
+
+// document.body.innerHTML = message;
+
+// user();
+
+
+//OR
+
+import * as demo from "./main1.js"
+
+console.log(demo.message);
+
+document.body.innerHTML = demo.message;
+
+demo.user()
---
+++ JavaScript/Module/main1.js
@@ -0,0 +1,18 @@
+// export let message = "ES6 Module";
+
+// export function user()
+// {
+//     console.log("Hello");
+// }
+
+
+//OR
+
+let message = "ES6 Module";
+
+function user()
+{
+    console.log("Hello");
+}
+
+export{message, user}
